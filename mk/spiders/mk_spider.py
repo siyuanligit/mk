bce8c34609d05d5a0a80888ec1c96452d0c1e1aa
Siyuan Li <sli920514@gmail.com>
2018-10-27 21:41:57 -0400

    re-arrange code, handle exception, add comments

--- mk/spiders/mk_spider.py
+++ mk/spiders/mk_spider.py
@@ -36,36 +36,40 @@ class mkSpider(Spider):
     ### parse detail page information ###
     def parse_detail_page(self, response):
 
+        # product listing name
         name = response.xpath('//h4[@class="name"]/text()').extract_first()
+        # product img
         productimg = "".join(["https://mechanicalkeyboards.com/shop/", response.xpath('//a[@class="zoom"]/img/@src').extract_first()])
-
+        # switch available
         switches = response.xpath('//td[@class="switch"]/a/text()').extract()
+        # sku - id
         sku = response.xpath('//td[@class="switch"]/div/text()').extract()
+        # price for specific switch
         sprice = response.xpath('//td[@class="price"]/text()').extract()
-
+        # keycap spec
         keycap = "".join(response.xpath('//ul[@class="acc_features"]/li//text()').extract()[:3])
-
+        # spec list
         specname = response.xpath('//td[@class="field"]/text()').extract()
         specvalue = response.xpath('//td[@class="value"]/text()').extract()
         spec = {}
         for n,v in zip(specname, specvalue):
             spec[n] = v
-
-        try:
-            averating = response.xpath('//div[@class="rating"]/img/@alt').extract_first().split()[0]
-        except AttributeError:
-            averating = None
-
-        try:
-            nreview = re.findall(r'\d+', response.xpath('//div[@class="rating"]/text()').extract_first())[2]
-        except TypeError:
+        # number of rating for the product and average rating
+        if response.xpath('//div[@class="rating"]/text()').extract_first() == None:
             nreview = 0
-
-        if nreview != 0:
+            averating = None
+        else:
+            nreview = re.findall(r'\d+', response.xpath('//div[@class="rating"]/text()').extract_first())[2]
+            averating = response.xpath('//div[@class="rating"]/img/@alt').extract_first().split()[0]
+            # review sections
+            # reviewer
             reviewers = response.xpath('//div[@class="review-metadata"]/div[@class="name"]/text()').extract()
             reviewers = list(map(lambda x: re.sub('(\\t|\\n|\\r| - )','', x), reviewers))[::2]
+            # time of review
             reviewtime = response.xpath('//div[@class="review-metadata"]/div[@class="name"]/span/text()').extract()
+            # review rating
             reviewrating = response.xpath('//div[@class="review-metadata"]/div[@class="queue"]/img/@src').extract()
+            # review text
             review = response.xpath('//div[@class="review-content"]/p').extract()
             review = list(map(lambda x: re.sub('(\\t|\\n|\\r|<br>|<p>|</p>)','', x), review))
 
