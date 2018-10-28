bf361428bd30801b4feeabbf019f904a96a0b03d
Siyuan Li <sli920514@gmail.com>
2018-10-27 22:50:20 -0400

    additional eda

--- test.r
+++ test.r
@@ -5,3 +5,7 @@ library(tidyverse)
 
 mkItems <- read_csv("mkItems.csv")
 mkItems %>% select(user, datetime, rating, review)
+
+mkItems %>% select(spec) %>% head(1) %>% pull() -> txt
+gsub("[^A-Za-z:,]", "", txt) %>% strsplit(",") -> txt
+txt[[1]]
